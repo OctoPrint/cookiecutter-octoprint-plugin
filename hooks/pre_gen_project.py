215ed90aa529d5260e784facb10212b1276f9fd9
Gina Häußge <gina@octoprint.org>
2025-06-04 11:13:27 +0200

    chore: adapt to modern tooling
    Now generates a pyproject.toml & a Taskfile

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,17 +1,34 @@
 import re
 import sys
 
-# Dashes '-' are technically allowed, but require special import and would generate invalid code with this template
-MODULE_REGEX = r"^[_a-zA-Z][_a-zA-Z0-9]+$"
+from packaging.licenses import canonicalize_license_expression, InvalidLicenseExpression
 
-# These names are used as class/module names in the template and therefore need to adhere to the given regex
-names_to_check = [
+#~~ Check for valid project name
+
+NAME_REGEX = r"^([A-Z0-9]|[A-Z0-9][A-Z0-9._-]*[A-Z0-9])$"
+plugin_name = "{{ cookiecutter.plugin_name }}"
+if not re.match(NAME_REGEX, plugin_name, flags=re.IGNORECASE):
+    print(f"ERROR: '{plugin_name}' is not a valid Python project name")
+    print("Please only use a-z, A-Z, underscores, dashs, dots and numbers (except for the first character)")
+
+#~~ Check for valid identifier and package names
+
+MODULE_REGEX = r"^[_A-Z][_A-Z0-9]+$"
+for name in [
     "{{ cookiecutter.plugin_identifier }}",
     "{{ cookiecutter.plugin_package }}",
-]
-
-for name in names_to_check:
-    if not re.match(MODULE_REGEX, name):
+]:
+    if not re.match(MODULE_REGEX, name, flags=re.IGNORECASE):
         print(f"ERROR: '{name}' is not a valid Python module name!")
         print("Please only use a-z, A-Z, underscores and numbers (except for the first character)")
         sys.exit(1)
+
+#~~ Check for valid SPDX license identifier
+
+plugin_license = "{{ cookiecutter.plugin_license }}"
+try:
+    canonicalize_license_expression(plugin_license)
+except InvalidLicenseExpression:
+    print(f"ERROR: '{plugin_license}' is not a valid SPDX license expression!")
+    print("Please refer to https://packaging.python.org/en/latest/specifications/license-expression/ for details on valid values")
+    sys.exit(1)
